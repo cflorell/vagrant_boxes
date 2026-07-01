1d1962c86cf6b676b072eee5d840aa96db081d44
Christoffer Florell <christoffer.florell@scania.com>
2026-07-01 13:27:01 +0200

    feat: Arch box
    EndeavourOS not suited for easy packer install.
    Thus arch instead.
    
    refs: vagrant_boxes#1

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -2,10 +2,4 @@
 
 BOX_NAME="ubuntu2404"
 BOX_FILE="ubuntu2404.vagrant.box"
-
-vagrant box remove "$BOX_NAME" --force || true
-curl -fsSL https://raw.githubusercontent.com/hashicorp/vagrant/main/keys/vagrant.pub -o vagrant.pub
-PACKER_LOG=1 PACKER_LOG_PATH="packer.log" packer build ubuntu.pkr.hcl
-vagrant box add --name "$BOX_NAME" "$BOX_FILE" --force
-
-echo "Box '$BOX_NAME' built and re-added to Vagrant."
+./build.sh
